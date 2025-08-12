7368022656854408a00ecb37987c49c3bda33d35
Jean Pool Gutierrez <jeanpoolgg@hotmail.com>
2025-08-12 12:18:22 -0500

    feat(state): extend bears store and integrate in UI

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -6,6 +6,8 @@ interface BearState {
     pandaBears: number;
 
     increaseBlackBears: (by: number) => void;
+    increasePolarBears: (by: number) => void;
+    increasePandaBears: (by: number) => void;
 }
 
 
@@ -13,7 +15,9 @@ export const useBearStore = create<BearState>()(set => ({
     blackBears: 10,
     polarBears: 5,
     pandaBears: 2,
-    increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by }))
+    increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
+    increasePolarBears: (by: number) => set((state) => ({ polarBears: state.polarBears + by })),
+    increasePandaBears: (by: number) => set((state) => ({ pandaBears: state.pandaBears + by })),
 
 
 
